8f34b37689cbdd3c0dfd2f4f84082c3e9c6fc2f9
Garrosh <taraslebedunsky@gmail.com>
2025-11-08 18:05:51 +0200

    fix: fix bug with unic

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import { config } from "./config";
+import { config } from "./config/index.js";
 
 app.listen(config.PORT, () => {
   console.log(`✅ Сервер запущено на порту ${config.PORT}`);
